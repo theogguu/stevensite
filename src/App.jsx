b3d4931ef0e1f2d40f3d382213c34bb963a651db
theogguu <stevengu.is.not@gmail.com>
2023-11-19 19:26:29 -0600

    basic homepage and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,33 @@
-import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap-icons/font/bootstrap-icons.css';
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+import HomePage from './components/HomePage/HomePage';
 
-const App = () => {
-  const [count, setCount] = useState(0);
 
+const Main = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test hot module replacement (HMR).
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="main">
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            {/* <Route path="/edit/:id" element={<EditFormForUrl profile={profile} courses={data.courses} />} /> */}
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
-};
+}
+
+// const queryClient = new QueryClient();
+
+const App = () => (
+  // <QueryClientProvider client={queryClient}>
+    <div className="container">
+      <Main />
+    </div>
+  // </QueryClientProvider>
+)
 
 export default App;
