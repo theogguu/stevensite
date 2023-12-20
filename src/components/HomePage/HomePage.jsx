5813acf6b2695ca0325b09ae44211c1ce865b3bb
theogguu <38506698+theogguu@users.noreply.github.com>
2023-12-20 12:32:26 -0500

    added a lot of modal content, styling changes for anchor

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -62,10 +62,26 @@ const HomePage = () => {
               users with instructions, materials, and purchase links given a query (e.g. "coffee table").'
               tags={["Flask", "JavaScript", "Python", "HTML", "CSS"]}
               onClick={() =>
-                openModalAndSetContent(
-                  { title: "Developer @ Crafty", 
-                    body: "" }
-                  )
+                openModalAndSetContent({
+                  title: "Crafty",
+                  body: [`Crafty is a GPT-powered AI assistant that assists users in their next woodworking project. Crafty works with users conversationally to help find a new project to build within their budget, then finds a blueprint online and parses it for the user. Upon further confirmation, Crafty links the user to where to buy the materials on Home Depot.`, 
+                  <br></br>,<br></br>, 
+                  <img src={'/crafty-1.png'} alt={"Crafty"} style={{maxWidth: "100%", height: "auto"}}/>,
+                  <br></br>,<br></br>,
+                  `I built Crafty with fellow student Royce Mettler as a project for our `,
+                  <i>Practicum in Information Systems </i>,
+                  `course. We used OpenAI’s API to handle conversations, providing each GPT call with a system context, the chat history, and external functions to call (and when to call them with `, 
+                  <a href='https://platform.openai.com/docs/guides/function-calling'>function calling</a>, 
+                  `). We built web-scraping functions with `, 
+                  <a href='https://pypi.org/project/beautifulsoup4/'>beautifulsoup4</a>,
+                  `, and scraped blueprints off of `,           
+                  <a href='https://learn.kregtool.com/projects-plans/search/?plansearch'>KregTool’s projects and plans</a>, 
+                  `. Finally, we used `,
+                  <a href='https://www.bigboxapi.com/'>BigBox’s Home Depot Product API </a>,
+                  `to find the web-scraped materials on Home Depot.`, 
+                  <br></br>,<br></br>,
+                  `The bulk of the back-end was written in Python, hosted on a Flask server, and the front-end was built with vanilla JavaScript, HTML, and CSS.`
+                ]})
               }
             />
             <Card
@@ -73,12 +89,12 @@ const HomePage = () => {
               time="July 2023 - September 2023"
               body="Designed Remix components adhering to design specifications to standardize future webpage development. Developed Flask-based APIs for front-end to MongoDB back-end interaction and automated MongoDB backups."
               href="https://www.newmusesproject.com/"
-              tags={["Remix", "MongoDB", "Flask", "JavaScript", "HTML", "CSS"]}
+              tags={["Remix", "React", "MongoDB", "Flask", "JavaScript", "HTML", "CSS"]}
               onClick={() =>
-                openModalAndSetContent(
-                  { title: "SWE Intern at New Muses Project", 
-                    body: "asdf" }
-                  )
+                openModalAndSetContent({
+                  title: "SWE Intern at New Muses Project",
+                  body: [`Over the summer, I worked as a Software Engineer Intern at New Muses Project, a non-profit organization that aims to provide a platform for artists to share their work. I worked on a team of 3 developers to build a new website for the organization.`]
+                })
               }
             />
             <Card
@@ -101,18 +117,52 @@ const HomePage = () => {
                 "Python",
                 "JavaScript",
               ]}
+              onClick={() =>
+                openModalAndSetContent({
+                  title: "PhotoApp",
+                  body: [`This was a multi-tier PhotoApp built with Python, JavaScript and Node/Express.js, and AWS services (EC2, S3 and RDS). A user can use the provided Python client to connect with our JS webservice that then access the AWS services and returns the appropriate data. This is now deprecated (because AWS keeps charging us ), but I fully documented our project to show our proof of concept.`,
+                  <br></br>,<br></br>, 
+                  `A user connects to our webservice with the provided Python client (or not). By using the webservice API, they can create users, upload or download images, search for certain stats (like images associated with a user), and more. A user has to use a userid to upload a photo and their photos are uploaded into their own bucket.`,
+                  <br></br>,<br></br>, 
+                  `Here's a rough diagram of our architecture:`,
+                  <br></br>,<br></br>,
+                  <img src={'/photoapp-1.png'} alt={"photoapp"} style={{maxWidth: "100%", height: "auto"}}/>,
+                  <br></br>,<br></br>,
+                  `For more details (like web service API endpoints), `,
+                  <a href='https://github.com/theogguu/aws-photoapp' target="_blank">check out the GitHub repo!</a>
+                ]
+                })
+              }
             />
 
             <Card
-              title="Developer • Campus Kitchens NU"
+              title="Full Stack Developer • Campus Kitchens NU"
               time="April 2023 - May 2023"
-              body="Led development on a full-stack web application using React frontend and Node.js/Express backend to manage food inventory, shift records, and user records for Northwestern University’s Campus Kitchen."
+              body="Developed on a full-stack web application using React frontend and Node.js/Express backend to manage food inventory, shift records, and user records for Northwestern University’s Campus Kitchen."
               tags={["AWS", "React", "JavaScript", "HTML", "CSS", "Bootstrap"]}
-            />
+              onClick={() =>
+                openModalAndSetContent({
+                  title: "Campus Kitchens NU",
+                  body: [
+                  <a href='https://sites.northwestern.edu/campuskitchen/' target='_blank'>Campus Kitchens NU</a>,
+                    ` is a student-led organization that aims to reduce food waste and food insecurity in the Evanston community by redistributing dining hall food that would otherwise be thrown away. A team of 4 developers and I built a full-stack web application to manage food inventory, shift records, and user records. We built the front-end with React and the back-end with Node.js/Express.js. We used AWS to host our webapp and database.`, 
+                  ,<br></br>,<br></br>, 
+                  ``,
+                    <br></br>,<br></br>, 
+                  `Here's a rough diagram of our architecture:`,
+                  <br></br>,<br></br>,
+                  <img src={'/photoapp-1.png'} alt={"photoapp"} style={{maxWidth: "100%", height: "auto"}}/>,
+                  <br></br>,<br></br>,
+                  `For more details (like web service API endpoints), `,
+                  <a href='https://github.com/theogguu/aws-photoapp' target="_blank">check out the GitHub repo!</a>
+                ]
+                })
+              }
+           />
 
             <Card
               title="Rapid Prototyping Projects"
-              body="Designed 3 standalone React webapps in a 6 week time period. Each webapp interacted with Firebase storage and databases."
+              body="Designed and deployed 3 standalone React webapps in a 6 week time period. Each webapp interacted with Firebase storage and databases and included features like user authorization, unit testing, and CI/CD."
               href="https://github.com/theogguu/stevensite-2"
               icon={
                 <IconButton
@@ -123,6 +173,8 @@ const HomePage = () => {
               tags={[
                 "Firebase",
                 "React",
+                "Vitest",
+                "CI/CD",
                 "JavaScript",
                 "HTML",
                 "CSS",
