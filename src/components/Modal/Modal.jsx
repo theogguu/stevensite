176cceca5f103851d8e56acd2bae6e8690c602e0
theogguu <38506698+theogguu@users.noreply.github.com>
2024-02-07 16:10:43 -0600

    Escape key closes modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,30 +1,42 @@
+import React, { useEffect } from 'react';
 import './Modal.css';
 import IconButton from '../IconButton/IconButton';
-// https://codebuckets.com/2021/08/08/bootstrap-modal-dialog-in-react-without-jquery/
 
-const Modal = ({open, close, children}) => (
-  <div
-    className={`modal ${open ? 'modal-show' : ''}`}
-    tabIndex="-1"
-    role="dialog"
-    onClick={(evt) => { if (evt.target === evt.currentTarget) close(); }}
-  >
-    {console.log({children})}
-    <div className="modal-dialog" role="document">
-      <div className="modal-content">
-        <div className="modal-header">
+const Modal = ({ open, close, children }) => {
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'Escape' && open) {
+        close();
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [open, close]);
+
+  return (
+    <div
+      className={`modal ${open ? 'modal-show' : ''}`}
+      tabIndex="-1"
+      role="dialog"
+      onClick={(evt) => {
+        if (evt.target === evt.currentTarget) close();
+      }}
+    >
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
             <h2 id="modal-title">{children.title}</h2>
-            <IconButton
-                icon="bi bi-x"
-                onClick={close}
-            />
-        </div>
-        <div className="modal-body">
-          {children.body}
+            <IconButton icon="bi bi-x" onClick={close} />
+          </div>
+          <div className="modal-body">{children.body}</div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Modal;
