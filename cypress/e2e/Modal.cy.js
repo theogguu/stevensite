f8ad0cd7d0ef7825dcce5e29ceb9164f5dddba0f
theogguu <38506698+theogguu@users.noreply.github.com>
2024-07-26 08:10:26 -0700

    fixed cypress test

--- cypress/e2e/Modal.cy.js
+++ cypress/e2e/Modal.cy.js
@@ -4,7 +4,7 @@ describe ('Test Card + Modal interaction', () => {
     it ('clicking on a card opens a modal', () => {
       cy.visit ('/');
       cy.get('[data-cy=modal]').should('have.css', 'visibility', 'hidden');
-      cy.get('[data-cy=project-card-0]').click();
+      cy.get('[data-cy=project-card-1]').click();
       cy.get('[data-cy=modal]').should('have.css', 'visibility', 'visible');
       cy.get('[data-cy=modal-close-btn]').click();
       cy.get('[data-cy=modal]').should('have.css', 'visibility', 'hidden');
@@ -12,21 +12,21 @@ describe ('Test Card + Modal interaction', () => {
 
     it ('clicking on close-btn should close the modal', () => {
       cy.visit ('/');
-      cy.get('[data-cy=project-card-0]').click();
+      cy.get('[data-cy=project-card-1]').click();
       cy.get('[data-cy=modal-close-btn]').click();
       cy.get('[data-cy=modal]').should('have.css', 'visibility', 'hidden');
     });
 
     it ('pressing the escape key should close the modal', () => {
       cy.visit ('/');
-      cy.get('[data-cy=project-card-0]').click();
+      cy.get('[data-cy=project-card-1]').click();
       cy.get('body').type('{esc}');
       cy.get('[data-cy=modal]').should('have.css', 'visibility', 'hidden');
     });
 
     it ('clicking outside the modal should close it', () => {
       cy.visit ('/');
-      cy.get('[data-cy=project-card-0]').click();
+      cy.get('[data-cy=project-card-1]').click();
       cy.get('[data-cy=modal]').click('topRight');
       cy.get('[data-cy=modal]').should('have.css', 'visibility', 'hidden');
     });
