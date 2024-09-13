fe955c7a40d5f4c83e1d410d77b80bf0526609b1
theogguu <38506698+theogguu@users.noreply.github.com>
2024-09-13 07:57:52 -0700

    fixed dead icon buttons

--- src/components/IconButton/IconButton.jsx
+++ src/components/IconButton/IconButton.jsx
@@ -1,11 +1,23 @@
 import './IconButton.css'
 
-const IconButton = ({ icon, href, alt, color="white", onClick}) => (
-    <div className="icon-button" onClick={onClick}>
-        <button aria-label={alt} href={href} target="_blank">
-            <i className={icon}></i>
-        </button>
-    </div>
-)
+const IconButton = ({ icon, href, alt, color="white", onClick }) => {
+    // Determine the element type based on the presence of `href`
+    const ElementType = href ? 'a' : 'button';
+
+    return (
+        <div className="icon-button">
+            <ElementType
+                aria-label={alt}
+                href={href}
+                target={href ? "_blank" : undefined}
+                rel={href ? "noopener noreferrer" : undefined}
+                onClick={onClick}
+                className="icon-button__link"
+            >
+                <i className={icon}></i>
+            </ElementType>
+        </div>
+    )
+}
 
 export default IconButton
